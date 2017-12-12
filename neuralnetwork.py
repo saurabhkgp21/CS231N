162feefdc457bcccf0c72a7ba434af0115242e7f
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2017-12-12 21:11:28 +0530

    MNIST using dropout

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -25,6 +25,7 @@ nOutput = 10
 learningRate = 0.0001
 epochs = 100
 batch_size = 100
+prob = 0.5
 weight = {
 	'W1': tf.Variable(tf.random_normal([nInput,nHidden1],stddev = 1/sqrt(nInput/2))),
 	'W2': tf.Variable(tf.random_normal([nHidden1,nHidden2],stddev = 1/sqrt(nHidden1/2))),
@@ -36,18 +37,22 @@ bias = {
 	'b2': tf.Variable(tf.zeros([nHidden2])),
 	'b3': tf.Variable(tf.zeros([nOutput]))
 }
+isTraining = tf.placeholder(tf.bool)
 
-z1 = tf.add(tf.matmul(x,weight['W1']),bias['b1'])
-a1 = tf.nn.relu(z1)
-z2 = tf.add(tf.matmul(a1,weight['W2']),bias['b2'])
-a2 = tf.nn.relu(z2)
-output = tf.add(tf.matmul(a2,weight['W3']),bias['b3'])
+a1 = tf.add(tf.matmul(x,weight['W1']),bias['b1'])
+z1 = tf.nn.relu(a1)
+z1 = tf.layers.dropout(z1,rate = prob, training = isTraining)
+a2 = tf.add(tf.matmul(a1,weight['W2']),bias['b2'])
+z2 = tf.nn.relu(a2)
+z2 = tf.layers.dropout(z2,rate = prob,training = isTraining)
+output = tf.add(tf.matmul(z2,weight['W3']),bias['b3'])
 
 cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = output,labels = y))
 train = tf.train.AdamOptimizer(learningRate).minimize(cost)
 
 predict = tf.argmax(tf.nn.softmax(output),1)
 accuracy = 100.0*tf.reduce_mean(tf.cast(tf.equal(predict,y),tf.float32))
+
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
@@ -58,18 +63,21 @@ with tf.Session() as sess:
 			yT = yTrain[indices]
 			_,c,acc = sess.run([train,cost,accuracy],feed_dict = {
 					x:xT,
-					y:yT
+					y:yT,
+					isTraining:True
 				})
 		if (e+1)%100 == 0 or 1:
 			c,acc = sess.run([cost,accuracy],feed_dict = {
 					x:xTrain,
-					y:yTrain
+					y:yTrain,
+					isTraining:False
 				})
 			print("At epoch {} ".format(e+1))
 			print("Train cost {}, accuracy {} ".format(c,acc))
 			c,acc = sess.run([cost,accuracy],feed_dict = {
 					x:xValid,
-					y:yValid
+					y:yValid,
+					isTraining:False
 				})
 			print("Validation cost {}, accuracy {}".format(c,acc))
 		
