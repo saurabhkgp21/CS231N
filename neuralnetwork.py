661cdd9776eb6bef5d4398eae29a64a8b63afce3
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2017-12-12 19:09:33 +0530

    Correction in MNIST

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -19,38 +19,52 @@ x = tf.placeholder('float',shape = [None,784])
 y = tf.placeholder(tf.int64,shape = [None,])
 
 nInput = 784
-nHidden = 50
+nHidden1 = 100
+nHidden2 = 50
 nOutput = 10
-learningRate = 0.01
-epochs = 2000
+learningRate = 0.0001
+epochs = 100
+batch_size = 100
 weight = {
-	'W1': tf.Variable(tf.random_normal([nInput,nHidden])),
-	'W2': tf.Variable(tf.random_normal([nHidden,nOutput]))
+	'W1': tf.Variable(tf.random_normal([nInput,nHidden1],stddev = 1/sqrt(nInput/2))),
+	'W2': tf.Variable(tf.random_normal([nHidden1,nHidden2],stddev = 1/sqrt(nHidden1/2))),
+	'W3': tf.Variable(tf.random_normal([nHidden2,nOutput],stddev = 1/sqrt(nHidden2/2)))
 }
 
 bias = {
-	'b1': tf.Variable(tf.zeros([nHidden])),
-	'b2': tf.Variable(tf.zeros([nOutput]))
+	'b1': tf.Variable(tf.zeros([nHidden1])),
+	'b2': tf.Variable(tf.zeros([nHidden2])),
+	'b3': tf.Variable(tf.zeros([nOutput]))
 }
 
 z1 = tf.add(tf.matmul(x,weight['W1']),bias['b1'])
 a1 = tf.nn.relu(z1)
-output = tf.add(tf.matmul(a1,weight['W2']),bias['b2'])
+z2 = tf.add(tf.matmul(a1,weight['W2']),bias['b2'])
+a2 = tf.nn.relu(z2)
+output = tf.add(tf.matmul(a2,weight['W3']),bias['b3'])
 
 cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = output,labels = y))
 train = tf.train.AdamOptimizer(learningRate).minimize(cost)
 
-predict = tf.argmax(output,1)
+predict = tf.argmax(tf.nn.softmax(output),1)
 accuracy = 100.0*tf.reduce_mean(tf.cast(tf.equal(predict,y),tf.float32))
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 	for e in xrange(epochs):
-		_,c,acc = sess.run([train,cost,accuracy],feed_dict = {
-				x:xTrain,
-				y:yTrain
-			})
-		if (e+1)%100 == 0:
+		for _ in xrange(numTrain/batch_size):
+			indices = np.random.choice(np.arange(numTrain),batch_size)
+			xT = xTrain[indices]
+			yT = yTrain[indices]
+			_,c,acc = sess.run([train,cost,accuracy],feed_dict = {
+					x:xT,
+					y:yT
+				})
+		if (e+1)%100 == 0 or 1:
+			c,acc = sess.run([cost,accuracy],feed_dict = {
+					x:xTrain,
+					y:yTrain
+				})
 			print("At epoch {} ".format(e+1))
 			print("Train cost {}, accuracy {} ".format(c,acc))
 			c,acc = sess.run([cost,accuracy],feed_dict = {
